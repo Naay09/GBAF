5bc474dfe944389ac27147d9989d2634636546f6
Nay <naila.said@icloud.com>
2022-10-18 09:37:54 +0200

    Ajout des fichiers header et footer

---
+++ includes/header-off.php
@@ -0,0 +1,3 @@
+<header>
+    <a href="index.php" class="head-left"><img class="logo" src="img/logo-gbaf.png"></a>
+</header>
---
+++ includes/header-on.php
@@ -0,0 +1,14 @@
+
+<header>
+    <a href="index.php" class="head-left"><img class="logo" src="img/logo-gbaf.png"></a>
+            
+        <nav class="head-right">
+
+            <a href="#" class="welcome"><p>Bienvenue Utilisateur</p></a>
+
+            <a href="#">Mon Compte</a>    
+            
+            <a href="#">Deconnexion</a>
+            
+        </nav>
+</header>
