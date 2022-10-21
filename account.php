384089696391793797ad8a7d9f69cf38832d37c5
Nay <naila.said@icloud.com>
2022-10-21 18:18:27 +0200

    Récupération des infos compte via BDD

--- account.php
+++ account.php
@@ -19,33 +19,59 @@ if(!isset($_SESSION['user']))
         <a href="index.php"><img class="return" src="img/return.png"></a>
         
            <main class="main-off">
+
+           <?php
+
+           $username = $_SESSION['user'];
+
+                // On récupère tout le contenu de la table account dans $sqlQuery où le username = $_SESSION['user']
+                $sqlQuery = 'SELECT * FROM account WHERE username = ?';
+                $accountQuery = $dbConnection->prepare($sqlQuery);
+                $accountQuery->execute(array($username));
+                $account = $accountQuery->fetch();
+
+           ?>
         
-                       <p>MON COMPTE</p>
-                       
-                       <form method="post">
-                           <div class="champs"> 
+                <p>MON COMPTE</p>
+                
+                <form action="update_process.php" method="post">
+                    <div class="champs"> 
                         <label>Nom : <span class="asterisk">*</span></label>
-                            <input type="text" value="S" name="nom" required="">
-                        </div>
-                        <div class="champs">
+                            <input type="text" value="<?php echo $account['upnom'];?>" name="nom" required="">
+                    </div>
+                    <div class="champs">
                         <label>Prénom : <span class="asterisk">*</span></label>
-                            <input type="text" value="N" name="prenom" required="">
-                        </div>
-                        <div class="champs">
+                            <input type="text" value="<?php echo $account['upprenom'];?>" name="prenom" required="">
+                    </div>
+                    <div class="champs">
                         <label>Nom d'utilisateur : <span class="asterisk">*</span></label>
-                            <input type="text" value="Naay" name="username" required="">
-                        </div>
-                        <div class="champs">
+                            <input type="text" value="<?php echo $account['upusername'];?>" name="username" required="">
+                     </div>
+                    <div class="champs"> 
+                        <label for="question">Question Secrète : <span class="asterisk">*</span></label>
+                            <select class="question" name="upquestion">
+                                <option selected="" disabled=""><?php echo $account['question'];?></option>
+                                <option value="Quel est le nom de jeune fille de votre mère ?">Quel est le nom de jeune fille de votre mère ?</option>
+                                <option value="Quel est le nom de votre ville natale ?">Quel est le nom de votre ville natale ?</option>
+                                <option value="Quel est le nom de votre meilleur/e ami/e ?">Quel est le nom de votre meilleur/e ami/e ?</option>
+                            </select>
+                    </div>
+                    <div class="champs"> 
+                        <label>Réponse : <span class="asterisk">*</span></label>
+                            <input type="text" value="<?php echo $account['reponse'];?>" name="upreponse" required="">
+                    </div>
+                    <div class="champs">
                         <label>Mot de passe : <span class="asterisk">*</span></label>
                             <input type="password" placeholder="Entrer le mot de passe actuel pour confirmer les changements" name="password" required="">
-                        </div>
-        
-                           <input type="submit" class="submit" name="update " value="ENREGISTRER LES MODIFICATIONS">
+                    </div>
+
+                    <input type="submit" class="submit" name="update " value="ENREGISTRER LES MODIFICATIONS">
                 </form>
-              <p>Tout les champs avec un  <span class="asterisk">*</span> sont obligatoire !</p>
+              <p>Tout les champs avec un  <span class="asterisk">*</span> sont obligatoires !</p>
            </main>
 
-           <?php require ('includes/footer.php'); ?>
+     <?php require ('includes/footer.php'); ?>
      
 
-</body></html>
+</body>
+</html>
